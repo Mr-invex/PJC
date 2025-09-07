67a797d94b92f8370b59782e7b1b678c5e3bfb9d
Mr-invex <otmar_102015@hotmail.com>
2025-09-07 03:32:24 -0500

    por fin x2

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,6 +12,16 @@ const botonesCard = document.querySelectorAll(".btn-card");
 botonesCard.forEach(boton => {
   boton.addEventListener("click", () => {
     const link = boton.getAttribute("data-link");
-    window.open(link, "_blank"); 
+    window.open(link, "_blank");
   });
 });
+
+
+const form = document.getElementById("form-contacto");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  console.log("✅ Tu formulario fue enviado exitosamente");
+  form.reset();
+  alert("✅ Tu formulario fue enviado exitosamente");
+});
+
